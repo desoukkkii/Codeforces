3e2f9c8b94bca93160d103f2f9385241b2dad79c
Mohammed Desouki <desouki.infosec@gmail.com>
2026-07-04 22:16:52 +0300

    1

--- Tatar TV Show/main.cpp
+++ Tatar TV Show/main.cpp
@@ -1,30 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T;
     cin >> T;
 
-    while (T--) {
+    while (T--)
+    {
         int n, k;
         string s;
         cin >> n >> k >> s;
 
         bool ok = true;
 
-        for (int r = 0; r < k; r++) {
+        for (int r = 0; r < k; r++)
+        {
 
             int ones = 0;
 
-            for (int i = r; i < n; i += k) {
+            for (int i = r; i < n; i += k)
+            {
                 if (s[i] == '1')
                     ones++;
             }
 
-            if (ones % 2 == 1) {
+            if (ones % 2 == 1)
+            {
                 ok = false;
                 break;
             }
